72d3f3fa1e2c35d04f4112cf21ac5d8f7dfe005c
Afeez Adewumi <adewumilanre01@gmail.com>
2024-08-05 23:14:40 -0500

    Showing students how to use arrow functions to write shorter code for simple function

--- camelize.js
+++ camelize.js
@@ -26,3 +26,16 @@ function camelize(str) {
   }
   camelize("background-color")
 
+//Using arrow function
+
+const camelize = str =>
+  str
+    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+
+camelize("background-color")
